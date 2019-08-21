b01c08e1a5c40cd636742d8c5a79ce08fe7794bc
Lee-Ji-Hye <kim1223mi@gmail.com>
2019-08-21 19:12:49 +0900

    주차장 & 음식점 병합

--- src/main/java/com/team/smart/app/controller/ParkingControllerAPI.java
+++ src/main/java/com/team/smart/app/controller/ParkingControllerAPI.java
@@ -15,8 +15,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.team.smart.app.vo.ParkingBDVO;
 import com.team.smart.service.ParkingAndService;
+import com.team.smart.app.vo.ParkingBDVO;
 
 @Controller
 public class ParkingControllerAPI {
@@ -26,6 +26,28 @@ public class ParkingControllerAPI {
 	@Autowired
 	ParkingAndService p_service;
 	
+	//안드로이드 주차관리 메인레이아웃 지도 마커 정보 
+	@ResponseBody// spring에서 안드로이드로 데이터(json)을 보내기 위한 어노테이션
+	@RequestMapping(value="api/parking/ParkingInfo")
+	public Map<String, Object> ParkingInfo(HttpServletRequest req,Model model) {
+		log.info("ParkingInfo()");
+		
+		
+		List<ParkingBDVO> lst = p_service.getBuildingList(req, model);
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		if(lst != null) {
+			map.put("parkingDBs", lst);
+			map.put("result", "성공");
+		}else {
+			
+			map.put("result", "실패");
+		}
+		
+		return map;
+	}
+	
+	//주차장건물 정보 가져옴
 	@ResponseBody// spring에서 안드로이드로 데이터(json)을 보내기 위한 어노테이션
 	@RequestMapping(value="api/parking/ParkingbuidingInfo")
 	public Map<String, Object> ParkingbuidingInfo(HttpServletRequest req,Model model) {
@@ -39,7 +61,7 @@ public class ParkingControllerAPI {
 		Map<String, Object> map = new HashMap<String, Object>();
 		
 		if(vo != null) {
-			map.put("parkingDB", lst);
+			map.put("parkingDBs", lst);
 			map.put("result", "성공");
 		}else {
 			
@@ -49,7 +71,44 @@ public class ParkingControllerAPI {
 		return map;
 	}
 	
+	//안드로이드 주차관리 주차권 정보 가져오기
+	@ResponseBody// spring에서 안드로이드로 데이터(json)을 보내기 위한 어노테이션
+	@RequestMapping(value="api/parking/ParkingTicketInfo")
+	public Map<String, Object> ParkingTicketInfo(HttpServletRequest req,Model model) {
+		log.info("ParkingTicketInfo()");
+
+		List<ParkingBDVO> lst = p_service.getBuildingList(req, model);
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		if(lst != null) {
+			map.put("pTickets", lst);
+			map.put("result", "성공");
+		}else {
+			
+			map.put("result", "실패");
+		}
+		
+		return map;
+	}
 	
-	
+	//안드로이드 주차관리 관리자 전화번호만 가져오기
+	@ResponseBody// spring에서 안드로이드로 데이터(json)을 보내기 위한 어노테이션
+	@RequestMapping(value="api/parking/ParkingManageHpInfo")
+	public Map<String, Object> ParkingManageHpInfo(HttpServletRequest req,Model model) {
+		log.info("ParkingManageHpInfo()");
+
+		List<ParkingBDVO> lst = p_service.getBuildingList(req, model);
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		if(lst != null) {
+			map.put("parkingDBs", lst);
+			map.put("result", "성공");
+		}else {
+			
+			map.put("result", "실패");
+		}
+		
+		return map;
+	}
 	
 }
--- src/main/java/com/team/smart/persistence/ParkingAndDAOImpl.java
+++ src/main/java/com/team/smart/persistence/ParkingAndDAOImpl.java
@@ -1,10 +1,13 @@
 package com.team.smart.persistence;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.team.smart.app.vo.ParkingBDVO;
+import com.team.smart.app.vo.ParkingTicketVO;
 
 @Repository
 public class ParkingAndDAOImpl implements ParkingAndDAO{
@@ -13,9 +16,24 @@ public class ParkingAndDAOImpl implements ParkingAndDAO{
 	@Autowired
 	private SqlSession sqlSession;
 
+	//주차장 전체 건물 정보
 	@Override
 	public ParkingBDVO getBDInfo(String b_code) {
 		return sqlSession.selectOne("ParkingAndDAO.getBDInfo",b_code);
 	}
+
+	//하나의 건물정보 가져오기 
+	@Override
+	public List<ParkingBDVO> getBDList() {
+		return sqlSession.selectList("ParkingAndDAO.getBDList");
+	}
+
+	//하나의 건물 주차권 정보 가져오기
+	@Override
+	public List<ParkingTicketVO> getTicketList(String b_code) {
+		return sqlSession.selectList("ParkingAndDAO.getTicketList",b_code);
+	}
+	
+	
 	
 }
