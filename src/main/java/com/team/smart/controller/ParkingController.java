39e2446b13a64fb35e702c13330d9ed8b0f7b44b
eunjimimi22 <sain087@gmail.com>
2019-08-21 18:34:25 +0900

    주차 안드로이드 메인

--- src/main/java/com/team/smart/controller/ParkingController.java
+++ src/main/java/com/team/smart/controller/ParkingController.java
@@ -26,6 +26,28 @@ public class ParkingController {
 	@Autowired
 	ParkingAndService p_service;
 	
+	//안드로이드 주차관리 메인레이아웃 지도 마커 정보 
+	@ResponseBody// spring에서 안드로이드로 데이터(json)을 보내기 위한 어노테이션
+	@RequestMapping(value="api/parking/ParkingInfo")
+	public Map<String, Object> ParkingInfo(HttpServletRequest req,Model model) {
+		log.info("ParkingInfo()");
+		
+		
+		List<ParkingBDVO> lst = p_service.getBuildingList(req, model);
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		if(lst != null) {
+			map.put("parkingDBs", lst);
+			map.put("result", "성공");
+		}else {
+			
+			map.put("result", "실패");
+		}
+		
+		return map;
+	}
+	
+	//주차장건물 정보 가져옴
 	@ResponseBody// spring에서 안드로이드로 데이터(json)을 보내기 위한 어노테이션
 	@RequestMapping(value="api/parking/ParkingbuidingInfo")
 	public Map<String, Object> ParkingbuidingInfo(HttpServletRequest req,Model model) {
@@ -39,7 +61,47 @@ public class ParkingController {
 		Map<String, Object> map = new HashMap<String, Object>();
 		
 		if(vo != null) {
-			map.put("parkingDB", lst);
+			map.put("parkingDBs", lst);
+			map.put("result", "성공");
+		}else {
+			
+			map.put("result", "실패");
+		}
+		
+		return map;
+	}
+	
+	//안드로이드 주차관리 주차권 정보 가져오기
+	@ResponseBody// spring에서 안드로이드로 데이터(json)을 보내기 위한 어노테이션
+	@RequestMapping(value="api/parking/ParkingTicketInfo")
+	public Map<String, Object> ParkingTicketInfo(HttpServletRequest req,Model model) {
+		log.info("ParkingTicketInfo()");
+
+		List<ParkingBDVO> lst = p_service.getBuildingList(req, model);
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		if(lst != null) {
+			map.put("pTickets", lst);
+			map.put("result", "성공");
+		}else {
+			
+			map.put("result", "실패");
+		}
+		
+		return map;
+	}
+	
+	//안드로이드 주차관리 관리자 전화번호만 가져오기
+	@ResponseBody// spring에서 안드로이드로 데이터(json)을 보내기 위한 어노테이션
+	@RequestMapping(value="api/parking/ParkingManageHpInfo")
+	public Map<String, Object> ParkingManageHpInfo(HttpServletRequest req,Model model) {
+		log.info("ParkingManageHpInfo()");
+
+		List<ParkingBDVO> lst = p_service.getBuildingList(req, model);
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		if(lst != null) {
+			map.put("parkingDBs", lst);
 			map.put("result", "성공");
 		}else {
 			
--- src/main/java/com/team/smart/service/ParkingAndServiceImpl.java
+++ src/main/java/com/team/smart/service/ParkingAndServiceImpl.java
@@ -1,5 +1,8 @@
 package com.team.smart.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,7 @@ import org.springframework.ui.Model;
 
 import com.team.smart.persistence.ParkingAndDAO;
 import com.team.smart.vo.ParkingBDVO;
+import com.team.smart.vo.ParkingTicketVO;
 
 //주차관리 안드로이드 서비스
 @Service
@@ -16,15 +20,42 @@ public class ParkingAndServiceImpl implements ParkingAndService{
 	@Autowired
 	ParkingAndDAO p_dao;
 	
+	//안드로이드 메인페이지 지도 마커 정보 가져오기 ...
+	@Override
+	public List<ParkingBDVO> getBuildingList(HttpServletRequest req, Model model) {
+
+		List<ParkingBDVO> list = p_dao.getBDList();
+		
+		return list;
+	}
+	
 	//지정한 주차장 건물 정보 가져오기 
 	@Override
 	public ParkingBDVO getBuildingInfo(HttpServletRequest req, Model model) {
 		
-		//String b_code = req.getParameter("b_code");
-		String b_code = "bd001";
+		String b_code = req.getParameter("b_code");
+		
 		ParkingBDVO vo = p_dao.getBDInfo(b_code);
 		
 		return  vo;
+	}
+	
+	//주차권 정보 가져오기
+	@Override
+	public List<ParkingTicketVO> getTicketInfo(HttpServletRequest req, Model model) {
+		
+		String b_code = req.getParameter("b_code");
+		List<ParkingTicketVO> list = p_dao.getTicketList(b_code);
+		
+		return list;
+	}
+
+	@Override
+	public List<ParkingBDVO> getManagerHpInfo(HttpServletRequest req, Model model) {
+		
+		String b_code = req.getParameter("b_code");
+		List<ParkingTicketVO> list = p_dao.getTicketList(b_code);
+		return null;
 	}
 
 }
